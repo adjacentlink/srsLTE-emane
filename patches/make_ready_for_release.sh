26d0b3774d4be98b2db3b10c7c1ea6ae77b1660e
Eric Schreiber <eschreiber@adjacentlink.com>
2020-10-12 18:19:31 -0400

    Remove srsenb_single_cpu_deadlock_fix.patch from set of make and unmake for release patches. Require more than 1 cpu to run an lte node.

--- patches/make_ready_for_release.sh
+++ patches/make_ready_for_release.sh
@@ -10,7 +10,4 @@ patch -p1 -i ./patches/srslte_emane_minimal_install.patch
 echo "apply packaging patch"
 patch -p1 -i ./patches/srslte_emane_package.patch
 
-echo "apply single cpu deadlock patch"
-patch -p1 -i ./patches/srsenb_single_cpu_deadlock_fix.patch
-
 echo "done"
--- patches/undo_make_ready_for_release.sh
+++ patches/undo_make_ready_for_release.sh
@@ -10,7 +10,4 @@ patch -p1 -i ./patches/srslte_emane_minimal_install.patch -R
 echo "remove packaging patch"
 patch -p1 -i ./patches/srslte_emane_package.patch -R
 
-echo "remove single cpu deadlock patch"
-patch -p1 -i ./patches/srsenb_single_cpu_deadlock_fix.patch -R
-
 echo "done"
